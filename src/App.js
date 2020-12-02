beb9361a8cf0f313d547273468eb584c72efc222
Rasmus Grønbek <groenbek@live.dk>
2020-12-02 12:08:07 +0100

    frontend profile page

--- src/App.js
+++ src/App.js
@@ -1,211 +1,313 @@
-import './App.css';
+import "./App.css";
 import React, { useState, useEffect } from "react";
-import DoLogin from "./login.js"
+import DoLogin from "./login.js";
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    NavLink,
-    useParams,
-    useLocation,
-    useHistory
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+  useParams,
+  useLocation,
+  useHistory,
 } from "react-router-dom";
-import facade from './apiFacade';
+import facade from "./apiFacade";
 
 function App() {
-    const [isLoggedIn, setLoggedIn] = useState(false)
-    let history = useHistory();
-    const goHome = () => {
-        history.push("/");
-    }
-    return (
-        <div>
-            <Header loginMsg={
-                isLoggedIn ? "Logout" : "Login"
-            }
-                isLoggedIn={isLoggedIn} />
-            <Switch>
-                <Route exact path="/">
-                    <Home />
-                </Route>
-                <Route exact path="/login">
-                    <DoLogin loggedIn={isLoggedIn} setLoggedIn={setLoggedIn} goHome={goHome} />
-                </Route>
-                <Route exact path="/movieSearch">
-                    <MovieSearch />
-                </Route>
-                <Route>
-                    <NoMatch />
-                </Route>
-            </Switch>
-        </div>
-
-    );
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  let history = useHistory();
+  const goHome = () => {
+    history.push("/");
+  };
+  return (
+    <div>
+      <Header
+        loginMsg={isLoggedIn ? "Logout" : "Login"}
+        isLoggedIn={isLoggedIn}
+      />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/login">
+          <DoLogin
+            loggedIn={isLoggedIn}
+            setLoggedIn={setLoggedIn}
+            goHome={goHome}
+          />
+        </Route>
+        <Route exact path="/movieSearch">
+          <MovieSearch />
+        </Route>
+        {isLoggedIn && (
+          <React.Fragment>
+            <Route exact path="/profilePage">
+              <ProfilePage />
+            </Route>
+          </React.Fragment>
+        )}
+        <Route>
+          <NoMatch />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 function Header({ isLoggedIn, loginMsg }) {
-    return (
-        <ul className="header">
-            <li>
-                <NavLink exact activeClassName="active" to="/">Home</NavLink>
-            </li>
-            <li>
-                <NavLink activeClassName="active" to="/movieSearch">Movie Surge</NavLink>
-            </li>
-            <li>
-                <NavLink activeClassName="active" to="/login">
-                    {loginMsg}</NavLink>
-            </li>
+  return (
+    <ul className="header">
+      <li>
+        <NavLink exact activeClassName="active" to="/">
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink activeClassName="active" to="/movieSearch">
+          Movie Search
+        </NavLink>
+      </li>
+      {isLoggedIn && (
+        <li>
+          <NavLink activeClassName="active" to="/profilePage">
+            Profile Page
+          </NavLink>
+        </li>
+      )}
 
-        </ul>
-
-    )
+      <li>
+        <NavLink activeClassName="active" to="/login">
+          {loginMsg}
+        </NavLink>
+      </li>
+    </ul>
+  );
 }
 
-
 function Home() {
-    return (
-        <div>
-            <h2>Home</h2>
-            <h3>How to use:</h3>
-            <ul>
-                <li>Login as User or Admin using username and password made in backend process</li>
-                <li>User and Admin use different endpoints for login</li>
-                <li><b>Extern API</b> use five different extern REST API endpoints through backend.</li>
-                <li>Make sure to have backend running locally or delpoyet, and adjust link(s) in <b>settings.js</b></li>
-                <li>Link to backend startcode: <a href="https://github.com/SebastianBentley/3SemProject">Backend Startcode</a> </li>
-            </ul>
-        </div>
-    )
+  return (
+    <div>
+      <h2>Home</h2>
+      <h3>How to use:</h3>
+      <ul>
+        <li>
+          Login as User or Admin using username and password made in backend
+          process
+        </li>
+        <li>User and Admin use different endpoints for login</li>
+        <li>
+          <b>Extern API</b> use five different extern REST API endpoints through
+          backend.
+        </li>
+        <li>
+          Make sure to have backend running locally or delpoyet, and adjust
+          link(s) in <b>settings.js</b>
+        </li>
+        <li>
+          Link to backend startcode:{" "}
+          <a href="https://github.com/SebastianBentley/3SemProject">
+            Backend Startcode
+          </a>{" "}
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 function MovieSearch() {
-    const [data, setData] = useState(null);
-    const [votes, setVotes] = useState({ upvotes: 0, downvotes: 0 });
-    const { strategy } = useParams();
-    const [movieTitle, setMovieTitle] = useState("");
-    const [isUpvoted, setIsUpvoted] = useState(false);
-    const [isDownvoted, setIsDownvoted] = useState(false);
+  const [data, setData] = useState(null);
+  const [votes, setVotes] = useState({ upvotes: 0, downvotes: 0 });
+  const { strategy } = useParams();
+  const [movieTitle, setMovieTitle] = useState("");
+  const [isUpvoted, setIsUpvoted] = useState(false);
+  const [isDownvoted, setIsDownvoted] = useState(false);
+  const [isSaved, setIsSaved] = useState(false);
 
-    function handleChange(event) {
-        const value = event.target.value;
-        setMovieTitle(value);
-    }
+  function handleChange(event) {
+    const value = event.target.value;
+    setMovieTitle(value);
+  }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    setData(null);
+    setVotes(null);
 
+    facade
+      .fetchMovieData(movieTitle)
+      .then((res) => setData(res))
+      .then()
+      .catch((err) => {
+        if (err.status) {
+          console.log(err.message);
+        }
+      });
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        setData(null);
-        setVotes(null);
+    facade
+      .getVotes(movieTitle)
+      .then((res) => setVotes(res))
+      .then()
+      .catch((err) => {
+        if (err.status) {
+          console.log(err.message);
+        }
+      });
+  }
 
+  function upvote(event) {
+    event.preventDefault();
+    facade
+      .upvote(data.Title)
+      .then()
+      .catch((err) => {
+        if (err.status) {
+          console.log(err.message);
+        }
+      });
+    setVotes({ upvotes: votes.upvotes + 1, downvotes: votes.downvotes });
+    setIsUpvoted(true);
+  }
 
+  function downvote(event) {
+    event.preventDefault();
+    facade
+      .downvote(data.Title)
+      .then()
+      .catch((err) => {
+        if (err.status) {
+          console.log(err.message);
+        }
+      });
+    setVotes({ upvotes: votes.upvotes, downvotes: votes.downvotes + 1 });
+    setIsDownvoted(true);
+  }
 
-        facade.fetchMovieData(movieTitle).then(res => setData(res)).then()
-            .catch(err => {
-                if (err.status) {
-                    console.log(err.message);
-                }
-            });
+  function addToSaved(event) {
+    event.preventDefault();
+    facade
+      .saveMovie(user, movieTitle)
+      .then()
+      .catch((err) => {
+        if (err.status) {
+          console.log(err.message);
+        }
+      });
+    setIsSaved(true);
+  }
 
-        facade.getVotes(movieTitle).then(res => setVotes(res)).then()
-            .catch(err => {
-                if (err.status) {
-                    console.log(err.message);
-                }
-            });
+  const upVoteButton = isUpvoted ? (
+    <button
+      disabled="disabled"
+      style={{ backgroundColor: "lightgreen" }}
+      onClick={upvote}
+    >
+      Upvote
+    </button>
+  ) : (
+    <button onClick={upvote}>Upvote</button>
+  );
 
+  const downVoteButton = isDownvoted ? (
+    <button disabled="disabled" onClick={downvote}>
+      Upvote
+    </button>
+  ) : (
+    <button onClick={downvote}>Downvote</button>
+  );
 
-    }
+  const savedMovie = isSaved ? (
+    <button onClick={addToSaved}>Save</button>
+  ) : (
+    <button onClick={addToSaved}> Saved </button>
+  );
 
-    function upvote(event) {
-        event.preventDefault();
-        facade.upvote(data.Title).then()
-            .catch(err => {
-                if (err.status) {
-                    console.log(err.message);
-                }
-            });
-        setVotes({ upvotes: votes.upvotes + 1, downvotes: votes.downvotes})
-        setIsUpvoted(true);
-    }
+  const toShow = data ? (
+    <div className="SearchResults">
+      <h3>{data.Title}</h3>
+      <div className="SearchRes1">
+        <p>
+          <b>Year:</b> {data.Year}
+        </p>
+        <p>
+          <b>Genre:</b> {data.Genre}
+        </p>
+        <p>
+          <b>Directors:</b> {data.Director}
+        </p>
+        <p>
+          <b>Writers:</b> {data.Writer}
+        </p>
+        <p>
+          <b>Actors:</b> {data.Actors}
+        </p>
+        <p>
+          <b>Description:</b> {data.Plot}
+        </p>
+      </div>
+      <div className="SearchRes2">
+        <img src={data.Poster}></img>
+        <p>
+          <b>User Rating: </b> yay: {votes.upvotes} nay: {votes.downvotes}
+        </p>
+        {upVoteButton}
+        {downVoteButton}
+        {savedMovie}
+        <p>
+          <b>Ratings:</b>{" "}
+        </p>
+        {data.Ratings.map((x) => (
+          <p key={x.Source}>
+            <b>{x.Source}</b> : {x.Value}
+          </p>
+        ))}
+        <p>
+          <b>Metascore:</b> {data.Metascore}
+        </p>
+        <p>
+          <b>imdbRating:</b> {data.imdbRating}
+        </p>
+        <p>
+          <b>imdbVotes:</b> {data.imdbVotes}
+        </p>
+        <p>
+          <b>imdbID:</b> {data.imdbID}
+        </p>
+      </div>
+    </div>
+  ) : (
+    ""
+  );
 
-    function downvote(event) {
-        event.preventDefault();
-        facade.downvote(data.Title).then()
-            .catch(err => {
-                if (err.status) {
-                    console.log(err.message);
-                }
-            });
-        setVotes({ upvotes: votes.upvotes, downvotes: votes.downvotes + 1 })
-        setIsDownvoted(true);
-    }
-
-    const upVoteButton = isUpvoted ? (
-        <button disabled="disabled" style={{ backgroundColor: "lightgreen" }} onClick={upvote}>Upvote</button>
-    ) : <button onClick={upvote}>Upvote</button>;
-
-    const downVoteButton = isDownvoted ? (
-        <button disabled="disabled" onClick={downvote}>Upvote</button>
-    ) : <button onClick={downvote}>Downvote</button>;
-
-    const toShow = data ? (
-
-        <div className="SearchResults">
-            <h3>{data.Title}</h3>
-            <div className="SearchRes1">
-                <p><b>Year:</b> {data.Year}</p>
-                <p><b>Genre:</b> {data.Genre}</p>
-                <p><b>Directors:</b> {data.Director}</p>
-                <p><b>Writers:</b> {data.Writer}</p>
-                <p><b>Actors:</b> {data.Actors}</p>
-                <p><b>Description:</b> {data.Plot}</p>
-            </div>
-            <div className="SearchRes2">
-                <img src={data.Poster}></img>
-                <p><b>User Rating: </b> yay: {votes.upvotes} nay: {votes.downvotes}</p>
-                {upVoteButton}
-                {downVoteButton}
-                <p><b>Ratings:</b> </p>
-                {data.Ratings.map(x =>
-                    <p key={x.Source}><b>{x.Source}</b> : {x.Value}</p>
-                )}
-                <p><b>Metascore:</b> {data.Metascore}</p>
-                <p><b>imdbRating:</b> {data.imdbRating}</p>
-                <p><b>imdbVotes:</b> {data.imdbVotes}</p>
-                <p><b>imdbID:</b> {data.imdbID}</p>
-
-            </div>
-
-
-        </div>
-    ) : ""
-
-    return (
-        <div>
-            <form className="Search">
-                <h2>Movie Search</h2>
-                <input placeholder="Movie title" id="movieSearch" value={movieTitle} onChange={handleChange} />
-                <button onClick={handleSubmit}>Search</button>
-            </form>
-            {toShow}
-        </div>
-    )
+  return (
+    <div>
+      <form className="Search">
+        <h2>Movie Search</h2>
+        <input
+          placeholder="Movie title"
+          id="movieSearch"
+          value={movieTitle}
+          onChange={handleChange}
+        />
+        <button onClick={handleSubmit}>Search</button>
+      </form>
+      {toShow}
+    </div>
+  );
 }
 
+function ProfilePage() {
+  return <div></div>;
+}
 
 function NoMatch() {
-    let location = useLocation();
-    return (
-        <div>
-            <h3>No match for
-                <code>{
-                    location.pathname
-                }</code>
-            </h3>
-        </div>
-    )
+  let location = useLocation();
+  return (
+    <div>
+      <h3>
+        No match for
+        <code>{location.pathname}</code>
+      </h3>
+    </div>
+  );
 }
 
 export default App;
--- src/apiFacade.js
+++ src/apiFacade.js
@@ -1,120 +1,138 @@
-import URL from "./settings.js"
+import URL from "./settings.js";
 
 function handleHttpErrors(res) {
-    if (!res.ok) {
-        return Promise.reject({ status: res.status, fullError: res.json() })
-    }
-    return res.json();
+  if (!res.ok) {
+    return Promise.reject({ status: res.status, fullError: res.json() });
+  }
+  return res.json();
 }
 
-function apiFacade() { /* Insert utility-methods from a latter step (d) here (REMEMBER to uncomment in the returned object when you do)*/
+function apiFacade() {
+  /* Insert utility-methods from a latter step (d) here (REMEMBER to uncomment in the returned object when you do)*/
 
-    const setToken = (token) => {
-        localStorage.setItem('jwtToken', token)
-    }
-    const getToken = () => {
-        return localStorage.getItem('jwtToken')
-    }
-    const loggedIn = () => {
-        const loggedIn = getToken() != null;
-        return loggedIn;
-    }
-    const logout = () => {
-        localStorage.removeItem("jwtToken");
-    }
+  const setToken = (token) => {
+    localStorage.setItem("jwtToken", token);
+  };
+  const getToken = () => {
+    return localStorage.getItem("jwtToken");
+  };
+  const loggedIn = () => {
+    const loggedIn = getToken() != null;
+    return loggedIn;
+  };
+  const logout = () => {
+    localStorage.removeItem("jwtToken");
+  };
 
+  const login = (user, password) => {
+    const options = makeOptions("POST", true, {
+      username: user,
+      password: password,
+    });
+    return fetch(URL + "/api/login", options)
+      .then(handleHttpErrors)
+      .then((res) => {
+        setToken(res.token);
+      });
+  };
 
-    const login = (user, password) => {
-        const options = makeOptions("POST", true, {
-            username: user,
-            password: password
-        });
-        return fetch(URL + "/api/login", options).then(handleHttpErrors).then(res => {
-            setToken(res.token)
-        })
+  const registerUser = (user, password) => {
+    const options = makeOptions("POST", true, {
+      username: user,
+      password: password,
+    });
+    return fetch(URL + "/api/login/createUser", options)
+      .then(handleHttpErrors)
+      .then((res) => {
+        setToken(res.token);
+      });
+  };
+
+  const fetchData = () => {
+    const options = makeOptions("GET", true); // True add's the token
+    var base64Url = getToken().split(".")[1];
+    var base64 = base64Url.replace("-", "+").replace("_", "/");
+    if (JSON.parse(atob(base64)).roles === "user") {
+      return fetch(URL + "/api/info/user", options).then(handleHttpErrors);
+    } else {
+      return fetch(URL + "/api/info/admin", options).then(handleHttpErrors);
     }
+  };
 
-    const registerUser = (user, password) => {
-        const options = makeOptions("POST", true, {
-            username: user,
-            password: password
-        });
-        return fetch(URL + "/api/login/createUser", options).then(handleHttpErrors).then(res => {
-            setToken(res.token)
-        })
+  const makeOptions = (method, addToken, body) => {
+    var opts = {
+      method: method,
+      headers: {
+        "Content-type": "application/json",
+        Accept: "application/json",
+      },
+    };
+    if (addToken && loggedIn()) {
+      opts.headers["x-access-token"] = getToken();
     }
-
-    const fetchData = () => {
-        const options = makeOptions("GET", true); // True add's the token
-        var base64Url = getToken().split('.')[1];
-        var base64 = base64Url.replace('-', '+').replace('_', '/');
-        if (JSON.parse(atob(base64)).roles === "user") {
-            return fetch(URL + "/api/info/user", options).then(handleHttpErrors);
-        } else {
-            return fetch(URL + "/api/info/admin", options).then(handleHttpErrors);
-        }
-
-        
-        
+    if (body) {
+      opts.body = JSON.stringify(body);
     }
+    return opts;
+  };
 
-   
-    const makeOptions = (method, addToken, body) => {
-        var opts = {
-            method: method,
-            headers: {
-                "Content-type": "application/json",
-                'Accept': 'application/json'
-            }
-        }
-        if (addToken && loggedIn()) {
-            opts.headers["x-access-token"] = getToken();
-        }
-        if (body) {
-            opts.body = JSON.stringify(body);
-        }
-        return opts;
-    }
+  const fetchMovieData = (title) => {
+    const options = makeOptions("GET", true); // True add's the token
+    return fetch(URL + "/api/movie/search/" + title, options).then(
+      handleHttpErrors
+    );
+  };
 
-    const fetchMovieData = (title) => {
-        const options = makeOptions("GET", true); // True add's the token
-        return fetch(URL + "/api/movie/search/" + title, options).then(handleHttpErrors);
-    }
+  const upvote = (title) => {
+    const options = makeOptions("POST", true, {
+      Title: title,
+    });
+    return fetch(URL + "/api/movie/rating/upvote", options).then(
+      handleHttpErrors
+    );
+  };
 
-    const upvote = (title) => {
-        const options = makeOptions("POST", true, {
-            Title: title
-        });
-        return fetch(URL + "/api/movie/rating/upvote", options).then(handleHttpErrors);
-    }
+  const downvote = (title) => {
+    const options = makeOptions("POST", true, {
+      Title: title,
+    });
+    return fetch(URL + "/api/movie/rating/downvote", options).then(
+      handleHttpErrors
+    );
+  };
 
-    const downvote = (title) => {
-        const options = makeOptions("POST", true, {
-            Title: title
-        });
-        return fetch(URL + "/api/movie/rating/downvote", options).then(handleHttpErrors);
-    }
+  const getVotes = (title) => {
+    const options = makeOptions("GET", true);
+    return fetch(URL + "/api/movie/rating/getrating/" + title, options).then(
+      handleHttpErrors
+    );
+  };
 
-    const getVotes = (title) => {
-        const options = makeOptions("GET", true);
-        return fetch(URL + "/api/movie/rating/getrating/" + title, options).then(handleHttpErrors);
-    }
+  const saveMovie = (user, title) => {
+    const options = makeOptions("POST", true, {
+      username: user,
+      Title: title,
+    });
+    return fetch(URL + "/api/info/user/addtosaved/", options).then(
+      handleHttpErrors
+    );
+  };
 
-
-    return {
-        makeOptions,
-        setToken,
-        getToken,
-        loggedIn,
-        login,
-        registerUser,
-        logout,
-        fetchData,
-        fetchMovieData,
-        upvote,
-        downvote,
-        getVotes
-    }
+  return {
+    makeOptions,
+    setToken,
+    getToken,
+    loggedIn,
+    login,
+    registerUser,
+    logout,
+    fetchData,
+    fetchMovieData,
+    upvote,
+    downvote,
+    getVotes,
+    saveMovie,
+  };
 }
 const facade = apiFacade();
 export default facade;
