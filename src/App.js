af8d972f68e4b6806936ab1fb98967b4c378e44a
Rasmus Grønbek <groenbek@live.dk>
2020-12-11 12:59:30 +0100

    Minor changes to homepage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import DoLogin from "./login.js";
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    NavLink,
-    useParams,
-    useLocation,
-    useHistory
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+  useParams,
+  useLocation,
+  useHistory,
 } from "react-router-dom";
 import facade from "./apiFacade";
 import MovieSearch from "./movieSearch.js";
@@ -19,167 +19,157 @@ import FeaturedMovies from "./featuredMovies.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-    const [isLoggedIn, setLoggedIn] = useState(false);
-    const [isAdmin, setIsAdmin] = useState(false);
-    let history = useHistory();
-    const goHome = () => {
-        history.push("/");
-    };
-    useEffect(() => {
-        if (isLoggedIn) {
-            setIsAdmin(facade.checkRole());
-        }
-    }, [isLoggedIn]);
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
+  let history = useHistory();
+  const goHome = () => {
+    history.push("/");
+  };
+  useEffect(() => {
+    if (isLoggedIn) {
+      setIsAdmin(facade.checkRole());
+    }
+  }, [isLoggedIn]);
 
-    return (
-        <div>
-            <Header loginMsg={
-                    isLoggedIn ? "Logout" : "Login"
-                }
-                isLoggedIn={isLoggedIn}
-                isAdmin={isAdmin}/>
-            <Switch>
-                <Route exact path="/">
-                    <Home/>
-                </Route>
-                <Route exact path="/login">
-                    <DoLogin loggedIn={isLoggedIn}
-                        setLoggedIn={setLoggedIn}
-                        goHome={goHome}/>
-                </Route>
-                <Route exact path="/movieSearch">
-                    <MovieSearch isLoggedIn={isLoggedIn}/>
-                </Route>
-                <Route exact path="/topMovies">
-                    <TopMovies/>
-                </Route>
+  return (
+    <div>
+      <Header
+        loginMsg={isLoggedIn ? "Logout" : "Login"}
+        isLoggedIn={isLoggedIn}
+        isAdmin={isAdmin}
+      />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/login">
+          <DoLogin
+            loggedIn={isLoggedIn}
+            setLoggedIn={setLoggedIn}
+            goHome={goHome}
+          />
+        </Route>
+        <Route exact path="/movieSearch">
+          <MovieSearch isLoggedIn={isLoggedIn} />
+        </Route>
+        <Route exact path="/topMovies">
+          <TopMovies />
+        </Route>
 
-                {
-                isLoggedIn && (
-                    <Route exact path="/profilePage">
-                        <ProfilePage/>
-                    </Route>
-                )
-            }
+        {isLoggedIn && (
+          <Route exact path="/profilePage">
+            <ProfilePage />
+          </Route>
+        )}
 
-                {
-                isAdmin && (
-                    <Route exact path="/adminPage">
-                        <AdminPage/>
-                    </Route>
-                )
-            }
-                <Route exact path="/featuredMovies">
-                    <FeaturedMovies/>
-                </Route>
-                <Route>
-                    <NoMatch/>
-                </Route>
-            </Switch>
-        </div>
-    );
+        {isAdmin && (
+          <Route exact path="/adminPage">
+            <AdminPage />
+          </Route>
+        )}
+        <Route exact path="/featuredMovies">
+          <FeaturedMovies />
+        </Route>
+        <Route>
+          <NoMatch />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
-function Header({isLoggedIn, loginMsg, isAdmin}) {
-    return (
-        <ul className="header">
-            <li>
-                <NavLink exact activeClassName="active" to="/">
-                    Home
-                </NavLink>
-            </li>
-            {
-            isLoggedIn && (
-                <li>
-                    <NavLink activeClassName="active" to="/profilePage">
-                        Profile Page
-                    </NavLink>
-                </li>
-            )
-        }
-            <li>
-                <NavLink activeClassName="active" to="/movieSearch">
-                    Movie Search
-                </NavLink>
-            </li>
+function Header({ isLoggedIn, loginMsg, isAdmin }) {
+  return (
+    <ul className="header">
+      <li>
+        <NavLink exact activeClassName="active" to="/">
+          Home
+        </NavLink>
+      </li>
+      {isLoggedIn && (
+        <li>
+          <NavLink activeClassName="active" to="/profilePage">
+            Profile Page
+          </NavLink>
+        </li>
+      )}
+      <li>
+        <NavLink activeClassName="active" to="/movieSearch">
+          Movie Search
+        </NavLink>
+      </li>
 
-            <li>
-                <NavLink activeClassName="active" to="/topMovies">
-                    Top Movies
-                </NavLink>
-            </li>
-            <li>
-                <NavLink activeClassName="active" to="/featuredMovies">
-                    Featured Movies
-                </NavLink>
-            </li>
-            <li>
-                <NavLink activeClassName="active" to="/login">
-                    {loginMsg}
-                    {" "} </NavLink>
-            </li>
-            {
-            isAdmin && isLoggedIn && (
-                <li>
-                    <NavLink activeClassName="active" to="/adminPage">
-                        Admin Page
-                    </NavLink>
-                </li>
-
-            )
-        }
-            <li style={{float: "right", margin: "10px"}}>
-                <form method="get" action="file.doc">
-                    <button className="btn-primary" type="submit">Get the app here!</button>
-                </form>
-            </li>
-        </ul>
-    );
+      <li>
+        <NavLink activeClassName="active" to="/topMovies">
+          Top Movies
+        </NavLink>
+      </li>
+      <li>
+        <NavLink activeClassName="active" to="/featuredMovies">
+          Featured Movies
+        </NavLink>
+      </li>
+      <li>
+        <NavLink activeClassName="active" to="/login">
+          {loginMsg}{" "}
+        </NavLink>
+      </li>
+      {isAdmin && isLoggedIn && (
+        <li>
+          <NavLink activeClassName="active" to="/adminPage">
+            Admin Page
+          </NavLink>
+        </li>
+      )}
+      <li style={{ float: "right", margin: "10px" }}>
+        <form method="get" action="file.doc">
+          <button className="btn-primary" type="submit">
+            Get the app here!
+          </button>
+        </form>
+      </li>
+    </ul>
+  );
 }
 
 function Home() {
-    return (
-        <div>
-            <h2>Home</h2>
-            <h3>How to use:</h3>
-            <ul>
-                <li>
-                    Login as User or Admin using username and password made in backend
-                                                  process
-                </li>
-                <li>User and Admin use different endpoints for login</li>
-                <li>
-                    <b>Extern API</b>
-                    use five different extern REST API endpoints through backend.
-                </li>
-                <li>
-                    Make sure to have backend running locally or deployed, and adjust
-                                                  link(s) in
-                    <b>settings.js</b>
-                </li>
-                <li>
-                    Link to backend startcode:{" "}
-                    <a href="https://github.com/SebastianBentley/3SemProject">
-                        Backend Startcode
-                    </a>
-                    {" "} </li>
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <h3>Welcome to our Movie Fetching Database</h3>
+      <br></br>
+      <ul>
+        Upon this homepage you get the possibility to search for whichever movie
+        you fancy, you can see the toprated movies based on user reviews. You
+        can even see some of our favorites, where we've added our
+        recommendations. Furthermore upon registration of a user or logging into
+        your already existing one, you can even like or dislike a movie{" "}
+        <b>AND</b> even adding the movies to your favorites so you will never
+        forget them.
+      </ul>
+      <br></br>
+      <ul>
+        But <b>HEY</b>, that isn't even it! You can even download our outsourced
+        app, mirroring some of the key features from the homepage, except now
+        you can always have it in your pocket, with you, at all times!{" "}
+        <b>WIN!</b>
+      </ul>
+      <br></br>
+      <br></br>
+      <br></br>
+    </div>
+  );
 }
 
 function NoMatch() {
-    let location = useLocation();
-    return (
-        <div>
-            <h3>
-                No match for
-                <code>{
-                    location.pathname
-                }</code>
-            </h3>
-        </div>
-    );
+  let location = useLocation();
+  return (
+    <div>
+      <h3>
+        No match for
+        <code>{location.pathname}</code>
+      </h3>
+    </div>
+  );
 }
 
 export default App;
